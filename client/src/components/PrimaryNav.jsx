b60fcd0a0712e73dfce01b69f0db3dc3ea12719d
gedelao <Gerardo_delao@ymail.com>
2022-07-21 13:21:29 -0500

    minor change

--- client/src/components/PrimaryNav.jsx
+++ client/src/components/PrimaryNav.jsx
@@ -80,13 +80,7 @@ export default function PrimaryNav() {
         <Anchor as={Link} borderBottom="1px" borderColor="red" py={3} px={3} to="/collaborators">
           Collaborators
         </Anchor>
-        <Anchor as={Link} borderBottom="1px" borderColor="red" py={3} px={3} to="/login">
-          Login
-        </Anchor>
-        <Anchor as={Link} borderBottom="1px" borderColor="red" py={3} px={3} to="/register">
-          Register
-        </Anchor>
-        <Anchor onClick={() => handleLogout()} borderBottom="1px" borderColor="red" py={3} px={3}>
+        <Anchor onClick={() => logout()} borderBottom="1px" borderColor="red" py={3} px={3}>
           Logout
         </Anchor>
       </Box>
